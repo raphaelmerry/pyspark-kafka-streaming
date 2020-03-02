3392a75359cbdbf6223cd4b2fdfab497ae9f974c
RaphaHell42 <37588468+RaphaHell42@users.noreply.github.com>
2020-03-03 00:05:57 +0100

    Updates to main3 and sparkConsumer
    * Switched Spark entry point from legacy SparkContext to SparkSession (introduced in v2.0.0)
    * Switched saving method from built-in Spark write.parquet to pyArrow parquet engine

--- Sources/main3.py
+++ Sources/main3.py
@@ -1,8 +1,19 @@
 from actors import sparkConsumer
+import pyarrow as pa
 import pyarrow.parquet as pq
 from pyspark.sql import Row
+from pyspark.sql.types import StructType, StructField, StringType
+from glob import glob
 import datetime
 import json
+import os
+
+
+def checkOutDirectories(topic2, topic3):
+    ''' Check that output directories exist '''
+    dir_list = ['Out', 'Out/'+topic2, 'Out/'+topic3]
+    for dir in dir_list:
+        if not glob(path+dir): os.mkdir(path+dir)
 
 
 def streamToParquet(rdd, topic, path):
@@ -11,9 +22,19 @@ def streamToParquet(rdd, topic, path):
     rows = [tuple(record.items()) for record in records]
     rows_rdd = sparkConsumer.sc.parallelize(rows)
     schema = None
+    type = None
     if topic == "Q2":
+        type = StructType([
+            StructField("source", StringType(), True),
+            StructField("word", StringType(), True),
+            StructField("topics", StringType(), True)
+        ])
         schema = rows_rdd.map(lambda x: Row(source=x[0][1], word=x[1][1], topics=x[2][1]))
     elif topic == "Q3":
+        type = StructType([
+            StructField("source", StringType(), True),
+            StructField("topic", StringType(), True)
+        ])
         schema = rows_rdd.map(lambda x: Row(source=x[0][1], topic=x[1][1]))
     else:
         print("Failed to save stream (topic {} not found).\n".format(topic))
@@ -25,22 +46,18 @@ def streamToParquet(rdd, topic, path):
     filename = filename.strftime("%Y-%m-%d_%H:%M:%S")
     filepath = path+"Out/"+topic+"/"+str(filename)+".parquet"
     print("Saving data from {} to {}".format(topic, filepath))
-    df = sparkConsumer.sqlContext.createDataFrame(schema)
-    df.show()
-    # PyArrow implementation not working yet
-    # pq.write_table(df, filepath, compression='snappy', flavor='spark')
-    df.write.parquet(filepath)
+    df = sparkConsumer.spark.createDataFrame(schema, type).toPandas()
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, filepath, compression='snappy')
 
 
 def main(path, topic2, topic3):
     '''
     sparkConsumer uses the createDirectStream approach to read streams from Kafka,
     as opposed to the receiver-based approach (createStream).
-    According to the Spark documentation:
-    The direct approach "periodically queries Kafka for the latest offsets in each topic+partition,
-    and accordingly defines the offset ranges to process in each batch".
-    Hence, the consumer defined in sparkConsumer reads from the last offset by default.
+    Hence, the consumer defined in sparkConsumer reads from the latest offset by default.
     '''
+    checkOutDirectories(topic2, topic3)
     q2_parsed = sparkConsumer.consumeStream(topic2)
     q2_parsed.foreachRDD(lambda rdd: streamToParquet(rdd, topic2, path))
     q3_parsed = sparkConsumer.consumeStream(topic3)
